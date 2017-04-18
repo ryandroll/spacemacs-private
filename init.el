dcf5093e4ebd11eba5d8fcf2b25e8a06c18293bd
Yung-Chuan Wang <ryandroll@gmail.com>
2017-04-18 15:06:54 +0800

    Change font surface to powerlevel

--- init.el
+++ init.el
@@ -180,9 +180,9 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 14
-                               :weight normal
+   dotspacemacs-default-font '("Sauce Code Powerline"
+                               :size 16
+                               :weight light
                                :width normal
                                :powerline-scale 1.1)
    ;; The leader key
